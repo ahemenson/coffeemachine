44c59d09c44cd0b8a5bf8ed73044a359ca37db7a
Aluno <me@me.com>
2014-07-28 09:03:49 -0300

    Task 1.02 finished for OO

--- src/main/java/br/ufpb/dce/aps/coffeemachine/impl/MyCoffeeMachine.java
+++ src/main/java/br/ufpb/dce/aps/coffeemachine/impl/MyCoffeeMachine.java
@@ -15,18 +15,18 @@ import br.ufpb.dce.aps.coffeemachine.Messages;
 public class MyCoffeeMachine implements CoffeeMachine {
 
 	private ComponentsFactory factory;
-	private int centavos, dolares; 
+	private int centavos, dolares;
 	private Display display;
 	private CashBox cashBox;
 	private Dispenser dispenser;
 	private ArrayList<Coin> moedas;
 	private Drink drink;
-	
+
 	public MyCoffeeMachine(ComponentsFactory factory) {
 		centavos = 0;
 		dolares = 0;
 		this.factory = factory;
-		display= factory.getDisplay();
+		display = factory.getDisplay();
 		display.info("Insert coins and select a drink!");
 		cashBox = factory.getCashBox();
 		moedas = new ArrayList<Coin>();
@@ -47,79 +47,71 @@ public class MyCoffeeMachine implements CoffeeMachine {
 	}
 
 	public void cancel() {
-		
-		if ((centavos==0) && (dolares==0)){
+
+		if ((centavos == 0) && (dolares == 0)) {
 			throw new CoffeeMachineException("");
 		}
-		
-		if(moedas.size()>0){
+
+		if (moedas.size() > 0) {
 			Coin[] inverso = Coin.reverse();
-			factory.getDisplay().warn("Cancelling drink. Please, get your coins.");
-			for(Coin r : inverso){
-				for(Coin aux : moedas){
-					if(aux == r){
+			factory.getDisplay().warn(Messages.CANCEL);
+			for (Coin r : inverso) {
+				for (Coin aux : moedas) {
+					if (aux == r) {
 						cashBox.release(aux);
 					}
 				}
 			}
 			factory.getDisplay().info("Insert coins and select a drink!");
 		}
-		
-	
 
-		
 	}
 
 	public void select(Drink drink) {
-		//verifyBlackPlan(inOrder);
+		// método verifyBlackPlan(inOrder);
 
-		factory.getCupDispenser().contains(1);  //inOrder.verify(cupDispenser).contains(1);
+		factory.getCupDispenser().contains(1); // inOrder.verify(cupDispenser).contains(1);
 
-		factory.getWaterDispenser().contains(0.5);  // inOrder.verify(waterDispenser).contains(anyDouble());
+		factory.getWaterDispenser().contains(0.5); // inOrder.verify(waterDispenser).contains(anyDouble());
 
-		factory.getCoffeePowderDispenser().contains (0.8);  // inOrder.verify(coffeePowderDispenser).contains(anyDouble());
+		factory.getCoffeePowderDispenser().contains(0.8); // inOrder.verify(coffeePowderDispenser).contains(anyDouble());
 
- 
-		if(drink == this.drink.BLACK_SUGAR){
+		if (drink == this.drink.BLACK_SUGAR) {
 
-		//inOrder.verify(sugarDispenser).contains(anyDouble());
-
-		factory.getSugarDispenser().contains(5.0);
+			factory.getSugarDispenser().contains(5.0); // inOrder.verify(sugarDispenser).contains(anyDouble());
 
 		}
 
-		 		 
+		// método verifyBlackSugarMix(InOrder inOrder) {
 
-		//verifyBlackSugarMix(InOrder inOrder) {
+		display.info("Mixing ingredients."); // inOrder.verify(display).info(Messages.MIXING);
 
-		display.info("Mixing ingredients."); //inOrder.verify(display).info(Messages.MIXING);
+		factory.getCoffeePowderDispenser().release(0.6); // inOrder.verify(coffeePowderDispenser).release(anyDouble());
 
-		factory.getCoffeePowderDispenser().release (0.6); // inOrder.verify(coffeePowderDispenser).release(anyDouble());
+		factory.getWaterDispenser().release(0.9); // inOrder.verify(waterDispenser).release(anyDouble());
 
-		factory.getWaterDispenser().release (0.9); // inOrder.verify(waterDispenser).release(anyDouble());
+		if (drink == this.drink.BLACK_SUGAR) {
 
-		 
-		if(drink == this.drink.BLACK_SUGAR){
+			// inOrder.verify(sugarDispenser).release(anyDouble());
 
-		//inOrder.verify(sugarDispenser).release(anyDouble());
-
-		factory.getSugarDispenser().release(5.0);
+			factory.getSugarDispenser().release(5.0);
 
 		}
 
-		 
-		//verifyDrinkRelease(InOrder inOrder)
+		// verifyDrinkRelease(InOrder inOrder)
 
-		display.info("Releasing drink."); //inOrder.verify(display).info(Messages.RELEASING);
+		display.info("Releasing drink."); // inOrder.verify(display).info(Messages.RELEASING);
 
-		factory.getCupDispenser().release (1);  // inOrder.verify(cupDispenser).release(1);
+		factory.getCupDispenser().release(1); // inOrder.verify(cupDispenser).release(1);
 
-		factory.getDrinkDispenser().release (0.3); //inOrder.verify(drinkDispenser).release(anyDouble());
+		factory.getDrinkDispenser().release(0.3); // inOrder.verify(drinkDispenser).release(anyDouble());
 
-		display.info("Please, take your drink.");  // inOrder.verify(display).info(Messages.TAKE_DRINK);
+		display.info("Please, take your drink."); // inOrder.verify(display).info(Messages.TAKE_DRINK);
 
 		display.info("Insert coins and select a drink!");
 		
-				
+		// Limpando o total de moedas inseridas
+		moedas.clear();
+
 	}
 }
