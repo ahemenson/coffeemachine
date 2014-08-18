a47447555f636ab69aa88603119693e938e4d552
Aluno <me@me.com>
2014-08-18 08:38:11 -0300

    Task 1.25 finished for OO

--- src/main/java/br/ufpb/dce/aps/coffeemachine/impl/WhiteCoffee.java
+++ src/main/java/br/ufpb/dce/aps/coffeemachine/impl/WhiteCoffee.java
@@ -10,12 +10,32 @@ import br.ufpb.dce.aps.coffeemachine.Messages;
 			super(factory);
 			
 	}
+	
+	public boolean blackPlan(){  
+		
+		if (!getFactory().getCupDispenser().contains(1)) { 
+			WarnMessage.setWarnMessage(Messages.OUT_OF_CUP);
+			return false;
+		}
+
+		if (!getFactory().getWaterDispenser().contains(80)) { 
+			WarnMessage.setWarnMessage(Messages.OUT_OF_WATER);
+			return false;
+		}
+
+		if (!getFactory().getCoffeePowderDispenser().contains(15)) {
+			WarnMessage.setWarnMessage(Messages.OUT_OF_COFFEE_POWDER); 
+			return false;
+		}
+		
+		return true;
+	}
 
 	public boolean whitePlan(){
 		if (!blackPlan()) { //verifyBlackPlan(getCupDispenser(), getWaterDispenser(), getCoffeePowderDispenser()
 			return false;
 		}
-		if(!getFactory().getCreamerDispenser().contains(2.0)){//inOrder.verify(creamerDispenser).contains(anyDouble());
+		if(!getFactory().getCreamerDispenser().contains(20)){//inOrder.verify(creamerDispenser).contains(anyDouble());
 			WarnMessage.setWarnMessage(Messages.OUT_OF_CREAMER);
 			return false;
 		}
@@ -24,9 +44,14 @@ import br.ufpb.dce.aps.coffeemachine.Messages;
 		return true;
 	}
 	
+	public void blackMix(){
+		getFactory().getCoffeePowderDispenser().release(15); 
+		getFactory().getWaterDispenser().release(80);
+	}
+	
 	public void whiteMix(){
 		blackMix();//blackMix(coffeePowderDispenser.release,waterDispenser).release )
-		getFactory().getCreamerDispenser().release(2.0); //inOrder.verify(creamerDispenser).release
+		getFactory().getCreamerDispenser().release(20); //inOrder.verify(creamerDispenser).release
 	} 
 	
 	
